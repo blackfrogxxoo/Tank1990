df20fc5c3301e79efe587305a94ca899e24fedaf
blackfrogxxoo <460929850@qq.com>
2014-10-31 23:20:13 +0800

    project

---
+++ src/org/wxc/tank1990/MenuView.java
@@ -0,0 +1,14 @@
+package org.wxc.tank1990;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class MenuView extends SurfaceView {
+
+	public MenuView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
